00c2d64519a7066376bab50aaf80cc7510e2c30a
OmegaDiamondX <mc.timofey@mail.ru>
2021-10-21 08:53:48 +0300

    Revert "Update DateTime"
    This reverts commit cbf816ff61db8b328eb152d3164b074fc2f29de2.

--- DateTime.pas
+++ DateTime.pas
@@ -1,14 +1,3 @@
 ﻿begin
-  
-  function IsLeapYear(n:integer):boolean;
-  begin
-    if n mod 4 = 0 then
-      if (n mod 100 = 0) and (n mod 400 <> 0) then
-        Result:=false
-      else
-        Result:=true;
-  end;
-  
-  function SecondsInMinutes():integer:=60;
-  
+
 end.
